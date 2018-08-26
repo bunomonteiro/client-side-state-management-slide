f8912c83fa66134bcdf817c36124594037e1d2a8
Bruno Monteiro <bunomonteiro@gmail.com>
2018-08-26 19:44:44 -0300

    Init

---
+++ script.js
@@ -0,0 +1,9 @@
+Reveal.initialize({
+  transition: 'convex', // none/fade/slide/convex/concave/zoom
+  fragmentInURL: false,
+  history: true,
+  // dependencies: [
+  //   { src: 'http://localhost:3001/socket.io/socket.io.js', async: true }, 
+  //   { src: 'plugin/remote-control/remote.js', async: true }
+  // ],
+});
